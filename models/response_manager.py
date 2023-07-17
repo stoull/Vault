41920206a7ce0b3f0e2e5c9e6772695b8347cf6c
stoull <stoull@users.noreply.github.com>
2023-07-17 22:09:06 +0800

    change json response codeing type

--- models/response_manager.py
+++ models/response_manager.py
@@ -11,7 +11,7 @@ class ResponseManager():
     # 生成 application/json 类型的响应
     @classmethod
     def json_response(cls, dict_data):
-        response = ResponseManager.app.response_class(response=json.dumps(dict_data),
+        response = ResponseManager.app.response_class(response=json.dumps(dict_data, ensure_ascii=False),
                                                       status=200,
-                                                      mimetype='application/json')
+                                                      mimetype='application/json;charset=utf-8')
         return response
