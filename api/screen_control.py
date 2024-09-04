f1f783da1bebfc652528703d2b13c67e22cd40c4
hut <changchunstep@126.com>
2024-09-04 20:30:09 +0800

    modified the logic of screen control

--- api/screen_control.py
+++ api/screen_control.py
@@ -6,18 +6,15 @@ from subprocess import run
 # External module imports
 import RPi.GPIO as GPIO
 
-from .smartclock_db_operator import insert_screen_action
-
 class ScreenControl:
-    def __init__(self):
+    def __init__(self, timeout=300):  # 默认超时时间为300秒（5分钟）
         self.detectedX = 0
         self.last_detection_time = time.time()
-
         self.timeout = self.get_timeout()
-        screen_state_int = self.current_screen_state()
-        self.screen_on = screen_state_int == 1
+        self.screen_on = ScreenControl.current_screen_state()
         self.running = False
         self.timer_thread = None
+        self.check_counter = 0
 
     def get_timeout(self):
         current_time = datetime.now().time()
@@ -43,49 +40,53 @@ class ScreenControl:
         if detected == 1:
             self.last_detection_time = time.time()
             if not self.screen_on:
-                self.turn_screen_on()
-            else:
-                print("screen is oning ")
+                self.change_screen_on()
 
     def check_timeout(self):
         while self.running:
             time_since_last_detection = time.time() - self.last_detection_time
-            if time_since_last_detection >= self.timeout:
-                if self.screen_on == False:
-                    print("该关屏幕了, 但屏幕本来就是关着的")
             if time_since_last_detection >= self.timeout and self.screen_on:
-                self.turn_screen_off()
+                self.change_screen_off()
+            # 每60秒检查一次屏幕状态,可能会被其它的程序改变
+            self.check_counter += 1
+            if self.check_counter >= 60:
+                self.screen_on = ScreenControl.current_screen_state()
+                self.check_counter = 0  # 重置计数器
             time.sleep(1)  # 每秒检查一次
 
     def change_screen_off(self):
-        result = ScreenControl.turn_screen_off()
-        if result == 0:
-            self.screen_on = False
-            insert_screen_action(False)
+        if self.screen_on == True:
+            if ScreenControl.turn_screen_off():
+                self.screen_on = False
 
     def change_screen_on(self):
-        if self.current_screen_state() != 1:
-            result = ScreenControl.turn_screen_on()
-            if result == 1:
+        if not self.screen_on:
+            if ScreenControl.turn_screen_on():
                 self.screen_on = True
-                insert_screen_action(True)
-            else:
-                print("该开屏幕了, 但屏幕没有开起来")
 
     @classmethod
     def turn_screen_off(cls):
         result = run('vcgencmd display_power 0', shell=True, capture_output=True, text=True)
-        return result.returncode
+        if result.stdout == 'display_power=0\n':
+            return True
+        else:
+            return False
 
     @classmethod
     def turn_screen_on(cls):
         result = run('vcgencmd display_power 1', shell=True, capture_output=True, text=True)
-        return result.returncode
+        if result.stdout == 'display_power=1\n':
+            return True
+        else:
+            return False
 
     @classmethod
     def current_screen_state(cls):
         result = run('vcgencmd display_power', shell=True, capture_output=True, text=True)
-        return result.returncode
+        if result.stdout == 'display_power=1\n':
+            return True
+        else:
+            return False
 
 class PIRDetector:
     def __init__(self, mode=GPIO.BCM, pin=4):  # 默认超时时间为300秒（5分钟）
@@ -103,6 +104,7 @@ class PIRDetector:
                 current_datetime = datetime.now()
                 cDateStr = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
                 value = GPIO.input(self.pin)
+#                 print(f"{cDateStr} value: ", value)
                 self.screen_controller.update_detection(value)
                 time.sleep(1)
         except KeyboardInterrupt:
@@ -113,6 +115,6 @@ class PIRDetector:
             GPIO.cleanup()
 
 if __name__ == "__main__":
-    print('检测中....')
+    print('检测中.......按Ctrl+C退出')
     detector = PIRDetector()
     detector.start_detect()
