1eb260c57317b929fe5802b32a774d2b111a08c3
hut <changchun.chen@growatt.com>
2025-10-31 14:19:29 +0800

    update scripts

--- update_nginx_cert.sh
+++ update_nginx_cert.sh
@@ -3,5 +3,5 @@ read -s -p "Enter sudo password: " PASSWORD
 echo
 scp ~/Downloads/ahut.site_nginx.zip hut@ahut.site:~/
 ssh hut@ahut.site "cd ~; SUDO_PASSWORD=$PASSWORD ./scripts/update_nginx_cert.sh;"
-unset SUDO_PASSWORD
+unset $PASSWORD
 ssh
